da0d8cf77c792d43f3700103fe44e75e034bfc0f
hiro21 <hiswing8@gmail.com>
2020-09-11 19:20:26 +0900

    testを修正

--- src/test/kotlin/com/example/demo/api/HelloControllerTest.kt
+++ src/test/kotlin/com/example/demo/api/HelloControllerTest.kt
@@ -1,14 +1,21 @@
 package com.example.demo.api
 
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-//internal class HelloControllerTest {
-//    @Autowired
-//    lateinit var controller: HelloController
-//    @Test
-//    fun test() {
-//        // assertEquals("hello", controller.fetch())
-//    }
-//}
+internal class HelloControllerTest {
+
+    private val mvc: MockMvc = MockMvcBuilders.standaloneSetup(HelloController()).build()
+
+    @Test
+    @Throws(Exception::class)
+    fun testGetOk() {
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("test"))
+    }
+}
