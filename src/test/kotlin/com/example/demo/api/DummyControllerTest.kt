1cec3b69446f4a23eba2b640eef27a443f2162c7
hiro21 <hiswing8@gmail.com>
2020-09-11 19:23:17 +0900

    dummyのテストを修正

--- src/test/kotlin/com/example/demo/api/DummyControllerTest.kt
+++ src/test/kotlin/com/example/demo/api/DummyControllerTest.kt
@@ -1,12 +1,20 @@
 package com.example.demo.api
 
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 internal class DummyControllerTest {
+
+    private val mvc: MockMvc = MockMvcBuilders.standaloneSetup(DummyController()).build()
+
     @Test
+    @Throws(Exception::class)
     fun test() {
-        val controller = DummyController()
-        assertEquals("dummy", controller.dummy())
+        mvc.perform(MockMvcRequestBuilders.get("/dummy"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("dummy"))
     }
 }
